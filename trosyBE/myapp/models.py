5a3074ca0a84b3a0959fc957e88477947b40bb34
Trond Haakon Trondsen <trondhtr@gmail.com>
2023-12-31 18:36:53 +0100

    Initial commit with Django project setup

--- trosyBE/myapp/models.py
+++ trosyBE/myapp/models.py
@@ -1,5 +1,30 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-class MyModel(models.Model):
-    field1 = models.CharField(max_length=100)
-    field2 = models.IntegerField()
+
+
+class MenstrualCycle(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    symptoms = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Cycle starting on {self.start_date} for {self.user.username}"
+    
+class TrainingSession(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField()
+    duration = models.DurationField()  # Could be in minutes or hours
+    intensity = models.CharField(max_length=50)  # e.g., Low, Medium, High
+    type = models.CharField(max_length=100)  # e.g., Cardio, Strength, Yoga
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.type} session on {self.date} for {self.user.username}"
+    
+
+
+    
